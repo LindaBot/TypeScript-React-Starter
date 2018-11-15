03c31c3cb93117ebd6fa994e458c5a830491ee92
LindaBot <voidnullbot@outlook.com>
2018-11-15 22:16:02 +1300

    Refactored Hello.tsx from stateless function component to stateful component

---
+++ src/components/Hello.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react'
+
+interface Props{
+    name: string,
+    enthusiasmLevel?: number 
+}
+interface State{
+    currentEnthusiasm: number
+}
+
+
+// This is a stateless function component
+class Hello extends React.Component<Props, State>{
+    constructor(props: Props){
+        super(props);
+        this.state = {
+            currentEnthusiasm: props.enthusiasmLevel || 1
+        }
+    }
+
+    onIncrement = () =>{
+        this.updateEnthusiasm(this.state.currentEnthusiasm + 1);
+    }
+
+    onDecrement = () =>{
+        this.updateEnthusiasm(this.state.currentEnthusiasm - 1);
+    }
+
+    
+    render(){
+        const { name } = this.props;
+
+        if (this.state.currentEnthusiasm <= 0) {
+            throw new Error('You could be a little more enthusiastic. :D');
+        }
+        
+        return (
+            <div className="Hello">
+                <div className="greeting">
+                    Hello {name + getExclamationMarks(this.state.currentEnthusiasm)}
+                </div>
+                <button onClick={this.onDecrement}>-</button>
+                <button onClick={this.onIncrement}>+</button>
+            </div>
+        );
+    }
+
+    updateEnthusiasm(currentEnthusiasm: number){
+        this.setState({ currentEnthusiasm })
+    }
+
+}
+
+export default Hello;
+
+function getExclamationMarks(numChars: number){
+    return Array(numChars + 1).join('!');
+}
